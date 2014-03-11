f1af4ec26d87b760a10c95bb275b4a0082623374
Tom Dryer <tomdryer.com@gmail.com>
2014-03-11 13:37:59 -0700

    Remove dead tests

--- test_editor.py
+++ test_editor.py
@@ -1,6 +1,6 @@
 import pytest
 
-from editor import Buffer, EditorGUI
+from editor import Buffer
 
 
 def test_buffer_init_from_text():
@@ -48,11 +48,9 @@ def test_buffer_set_text():
     b.set_text(0, 5, 1, 1, 'was\nthe')
     assert b.get_lines() == ['this was', 'the test']
 
-    #b = Buffer('this is\na test')
-    #b.set_text(0, 5, 0, 5, 'foo')
     b = Buffer('this foois\na test')
     b.set_text(0, 8, 0, 8, ' ')
-    assert b.get_lines() == ['this foo is', 'a test'] # FAILS
+    assert b.get_lines() == ['this foo is', 'a test']
 
 
 def test_buffer_set_text_negative_row():
@@ -85,56 +83,3 @@ def test_buffer_set_text_nonexistent_col():
         b.set_text(0, 4, 0, 0, 'a')
     with pytest.raises(ValueError):
         b.set_text(0, 0, 0, 4, 'a')
-
-
-def test_editorgui_wrap_text():
-    res = EditorGUI._get_wrapped_lines(['This is', 'a test.'], 5, 4)
-    assert list(res) == [
-        ['This ', 'is'],
-        ['a tes', 't.'],
-    ]
-    res = EditorGUI._get_wrapped_lines(['This is', 'a test.'], 5, 3)
-    assert list(res) == [
-        ['This ', 'is'],
-    ]
-    res = EditorGUI._get_wrapped_lines(['This is', 'a test.'], 5, 5)
-    assert list(res) == [
-        ['This ', 'is'],
-        ['a tes', 't.'],
-    ]
-    res = EditorGUI._get_wrapped_lines(['This is', ''], 5, 4)
-    assert list(res) == [
-        ['This ', 'is'],
-        [''],
-    ]
-
-#def test_buffer_insert():
-#    b = Buffer()
-#    b.insert((0, 0), 'test')
-#    assert b.get_lines() == ['test']
-#
-#    b = Buffer('Tm')
-#    b.insert((0, 1), 'o')
-#    assert b.get_lines() == ['Tom']
-#
-#    b = Buffer('hello\nworld')
-#    b.insert((0, 0), 'why ')
-#    assert b.get_lines() == ['why hello', 'world']
-#
-#    b = Buffer('hello\nworld')
-#    b.insert((0, 5), ' test')
-#    assert b.get_lines() == ['hello test', 'world']
-#
-#    b = Buffer('foo\nbaz')
-#    b.insert((0, 3), '\nbar\nbar')
-#    assert b.get_lines() == ['foo', 'bar', 'bar', 'baz']
-#
-#def test_buffer_delete():
-#    #b = Buffer()
-#    #b.delete((1, 1, 1, 1))
-#    #assert b.get_lines() == ['']
-#
-#    b = Buffer('bear')
-#    b.delete((0, 1, 0, 2))
-#    assert b.get_lines() == ['bar']
-
